e57c2380160350b76a5d4d03dd5603836e827abd
Igor Leandro Dantas Medeiros <141191598+IGTag@users.noreply.github.com>
2024-04-22 19:25:04 -0300

    Auth UI Complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+
 import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import "@/styles/globals.css";
+import RegisterModal from "@/components/modals/RegisterModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <Layout > 
-  <Component {...pageProps} />
+  <>
+  <RegisterModal />
+  <LoginModal />
+    <Layout > 
+      <Component {...pageProps} />
   </Layout>
+  </>
   );
 }
